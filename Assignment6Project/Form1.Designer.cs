8cdc740b88c0cf6a7281cf4484491bae738add36
Giovanni Martinez <151098531+Giomart1122@users.noreply.github.com>
2025-04-27 16:28:26 -0500

    password checker asm6

--- Assignment6Project/Form1.Designer.cs
+++ Assignment6Project/Form1.Designer.cs
@@ -1,38 +1,100 @@
-﻿namespace Assignment6Project;
-
-partial class Form1
+﻿namespace Assignment6Project
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private Label labelPrompt;
+        private TextBox textBoxInput;
+        private Button buttonCheckPassword;
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel statusLabel;
+        private CheckBox checkBoxShowPassword;
+        private ListBox listBoxCriteria;
+        private Button buttonGeneratePassword;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        private void InitializeComponent()
+        {
+            this.menuStrip = new MenuStrip();
+            this.fileToolStripMenuItem = new ToolStripMenuItem();
+            this.exitToolStripMenuItem = new ToolStripMenuItem();
+            this.labelPrompt = new Label();
+            this.textBoxInput = new TextBox();
+            this.buttonCheckPassword = new Button();
+            this.statusStrip = new StatusStrip();
+            this.statusLabel = new ToolStripStatusLabel();
+
+            // MenuStrip
+            this.menuStrip.Items.AddRange(new ToolStripItem[] { this.fileToolStripMenuItem });
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { this.exitToolStripMenuItem });
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new EventHandler(this.exitToolStripMenuItem_Click);
+
+            // Label
+            this.labelPrompt.Text = "Enter password:";
+            this.labelPrompt.Location = new System.Drawing.Point(30, 50);
+
+            // TextBox
+            this.textBoxInput.Location = new System.Drawing.Point(150, 50);
+            this.textBoxInput.Width = 200;
+            this.textBoxInput.TextChanged += new EventHandler(this.textBoxInput_TextChanged);
+
+            // Check pass button
+            //this.buttonCheckPassword.Text = "Check Password";
+            //this.buttonCheckPassword.Location = new System.Drawing.Point(150, 100);
+            //this.buttonCheckPassword.Click += new EventHandler(this.buttonCheckPassword_Click);
+
+            // StatusStrip
+            this.statusStrip.Items.Add(this.statusLabel);
+
+            // Form1
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.labelPrompt);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonCheckPassword);
+            this.Controls.Add(this.statusStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Text = "C# Password Checker and Generator";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ClientSize = new System.Drawing.Size(400, 300); //size of the app
+           
+            // Checkbox for Show Password
+            this.checkBoxShowPassword = new CheckBox();
+            this.checkBoxShowPassword.Text = "Show Password";
+            this.checkBoxShowPassword.Location = new System.Drawing.Point(150, 80);
+            this.checkBoxShowPassword.CheckedChanged += new EventHandler(this.checkBoxShowPassword_CheckedChanged);
+
+            // ListBox for Criteria
+            this.listBoxCriteria = new ListBox();
+            this.listBoxCriteria.Location = new System.Drawing.Point(150, 120);
+            this.listBoxCriteria.Size = new System.Drawing.Size(200, 80);
+            this.listBoxCriteria.Items.Add("• At least 6 characters");
+            this.listBoxCriteria.Items.Add("• Includes numbers");
+            this.listBoxCriteria.Items.Add("• Includes symbols");
+
+            // Button to Generate Password
+            this.buttonGeneratePassword = new Button();
+            this.buttonGeneratePassword.Text = "Generate Strong Password";
+            this.buttonGeneratePassword.Location = new System.Drawing.Point(150, 210);
+            this.buttonGeneratePassword.Click += new EventHandler(this.buttonGeneratePassword_Click);
+
+            // Update controls
+            this.Controls.Add(this.checkBoxShowPassword);
+            this.Controls.Add(this.listBoxCriteria);
+            this.Controls.Add(this.buttonGeneratePassword);
+
+        }
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- Assignment6Project/Form1.cs
+++ Assignment6Project/Form1.cs
@@ -1,9 +1,96 @@
-namespace Assignment6Project;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace Assignment6Project
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+    private void buttonGeneratePassword_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = GenerateStrongPassword();
+        }
+
+    private string GenerateStrongPassword()
+        {
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+            Random random = new Random();
+            char[] password = new char[12];
+            for (int i = 0; i < password.Length; i++)
+            {
+                password[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(password);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxInput.UseSystemPasswordChar = !checkBoxShowPassword.Checked;
+        }
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            EvaluatePasswordStrength(textBoxInput.Text);
+        }
+
+        private void buttonCheckPassword_Click(object sender, EventArgs e)
+        {
+            string password = textBoxInput.Text;
+            EvaluatePasswordStrength(password);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void EvaluatePasswordStrength(string password)
+        {
+            if (password.Length >= 6)
+                listBoxCriteria.Items[0] = "✔️ At least 6 characters";
+            else
+                listBoxCriteria.Items[0] = "• At least 6 characters";
+
+            if (Regex.IsMatch(password, @"\d"))
+                listBoxCriteria.Items[1] = "✔️ Includes numbers";
+            else
+                listBoxCriteria.Items[1] = "• Includes numbers";
+
+            if (Regex.IsMatch(password, @"[\W_]"))
+                listBoxCriteria.Items[2] = "✔️ Includes symbols";
+            else
+                listBoxCriteria.Items[2] = "• Includes symbols";
+
+            // Color feedback on strength
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                statusLabel.Text = "Please enter a password.";
+                statusLabel.ForeColor = System.Drawing.Color.Black;
+            }
+            else if (password.Length < 6)
+            {
+                statusLabel.Text = "Weak Password: Too short!";
+                statusLabel.ForeColor = System.Drawing.Color.Red;
+            }
+            else if (Regex.IsMatch(password, @"^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$"))
+            {
+                statusLabel.Text = "Moderate Password: Letters and numbers.";
+                statusLabel.ForeColor = System.Drawing.Color.Goldenrod;
+            }
+            else if (Regex.IsMatch(password, @"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).+$"))
+            {
+                statusLabel.Text = "Strong Password: Letters, numbers, and symbols!";
+                statusLabel.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                statusLabel.Text = "Password strength unclear.";
+                statusLabel.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
     }
 }
